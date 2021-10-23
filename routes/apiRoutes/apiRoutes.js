9d85ab891def845bd87be11c47718c054e839e40
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-10-23 11:17:11 -0400

    everything works now

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
-const notes = require("../../db/db.json");
+let notes = require("../../db/db.json");
 var uniqid = require('uniqid'); 
 
 const fs = require('fs');
 const path = require('path')
 
 router.get('/notes', (req, res) => {
-  res.send(notes);
+  fs.readFile(path.resolve(__dirname,"../../db/db.json"), "utf-8", (err, data)=> {
+    if (err) console.log(err.message)
+    console.log(data)
+    res.status(200).json(JSON.parse(data))
+  });
 });
 
 router.post('/notes', (req, res) => {
@@ -14,27 +18,30 @@ router.post('/notes', (req, res) => {
   // console.log(notes)
   // console.log(req.body)
 
-  notes.push(req.body)
-  var uniqid = require('uniqid'); 
-
-  console.log(uniqid());
-  console.log(uniqid(), uniqid()); 
+  var newnote = req.body 
+  newnote.id = uniqid()
+  notes.push(newnote)
+  
 
   // console.log(notes);
 
   fs.writeFileSync(path.join(__dirname,"../../db/db.json"), JSON.stringify(notes))
-
+  notes = require("../../db/db.json");
   res.send(notes)
 
 });
 
 router.delete("/notes/:id", (req, res) => {
-  const userIndex = getUserIndex(req.params.userId)
+  // const userIndex = getUserIndex(req.params.userId)
+  console.log(req.params.id)
 
-  if (userIndex === -1) return res.status(404).json({})
- 
-  users.splice(userIndex, 1)
-  res.json(users)
+  // if (userIndex === -1) return res.status(404).json({})
+ let newdata = notes.filter(note => note.id !== req.params.id)
+ console.log(newdata)
+  // users.splice(userIndex, 1)
+  // res.json(users)
+  fs.writeFileSync(path.join(__dirname,"../../db/db.json"), JSON.stringify(newdata))
+  res.status(200).json(newdata)
 });
 
 
